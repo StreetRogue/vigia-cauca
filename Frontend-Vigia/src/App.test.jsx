dda431d8bb80610b0635a000106a83c9c00a3877
StreetRogue <juanestebanchavezcollazos2004@gmail.com>
2026-04-02 01:47:34 -0500

    style: formatear código con Prettier

--- Frontend-Vigia/src/App.test.jsx
+++ Frontend-Vigia/src/App.test.jsx
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from "vitest";
 
-describe('App', () => {
-  it('should pass', () => {
+describe("App", () => {
+  it("should pass", () => {
     expect(true).toBe(true);
   });
 });
